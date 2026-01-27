bb6af2f0c0807c77708e46a77688ecf74755e774
Franco <caceresfranco481@gmail.com>
2026-01-26 22:30:09 -0300

    Implementacion de metodos se implementaron dos metodos
    primer metodo: la funcion que cumple el primer metodo implementado (conectar)es conectarse a la base de datos
    el cual devuelve un variable que tiene toda la base de datos asi despues se conecta con un cursor para poder hacer las query
    
    segundo metodo: la funcion que cumple el segundo metodo implementado (desconectar) es recibir como parametro esa variable que recibio
    en el main desde conectar para desconectarse de la base de datos y no utilice recursos de forma innescesario

---
+++ conexiones_db.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def conectar():
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Pilates28",
+            database="verano1_db"
+        )
+        if conexion.is_connected():
+           
+           return conexion
+    except mysql.connector.Error as fallo:
+        print (f"Error de conexion {fallo}")
+        return None
+
+def desconectar (conexion):
+    if  conexion and conexion.is_connected():
+        conexion.close()
