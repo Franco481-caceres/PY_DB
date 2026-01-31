e1d159e84e13b1d16cbcca6ed2eaae54423d2435
Franco <caceresfranco481@gmail.com>
2026-01-30 21:25:37 -0300

    Clases en conexiones_db se implemento el uso de clases las conexiones para en caso de tener que cambiar algo solo lo cambiamos desde el init

--- conexiones_db.py
+++ conexiones_db.py
@@ -1,19 +1,28 @@
 import mysql.connector
 
-def conectar():
-    try:
-        conexion = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Pilates28",
-            database="seguros"
-        )
-        if conexion.is_connected():
-           return conexion
-    except mysql.connector.Error as fallo:
-        print (f"Error de conexion {fallo}")
-        return None
 
-def desconectar (conexion):
-    if  conexion and conexion.is_connected():
-        conexion.close()
+class conexion:
+    def __init__(self):
+        self.conexion= None
+        self.cursor=None
+        self.host="localhost"
+        self.user="root"
+        self.password="Pilates28"
+        self.database="seguros"
+    def conectar(self):
+        try:
+            self.conexion = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            if self.conexion.is_connected():
+               return self.conexion
+        except mysql.connector.Error as fallo:
+            print (f"Error de conexion {fallo}")
+            return None
+
+    def desconectar (self):        
+        if  self.conexion and self.conexion.is_connected():
+            self.conexion.close()
